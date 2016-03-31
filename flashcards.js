06307c0c804ea1c4bdd99e5afedc6beaeae603e9
Wes Reed <wesley.a.reed@gmail.com>
2016-03-30 23:33:47 -0400

    Add card entry functionality with prototype checking for whitespace/empty string

--- flashcards.js
+++ flashcards.js
@@ -1,61 +1,60 @@
 var cardsAdmin = {
-  cards: [],
-  cardIndex: 0,
-  cardSide: 0,
-  cardButton:document.querySelector(".cardButton"),
-  cardText:document.querySelector(".cardText"),
-  cardPosition:document.querySelector(".positionIndex"),
-  cardCurrent: function() {
-    var currentCard = this.cards[this.cardIndex];
-    this.cardText.innerHTML = currentCard.display(this.cardSide);
-    this.cardPosition.innerHTML = (this.cardIndex + 1) + "/" + this.cards.length;
-  },
+    cards: [],
+    cardIndex: 0,
+    cardSide: 0,
+    cardButton: document.querySelector(".cardButton"),
+    cardText: document.querySelector(".cardText"),
+    cardPosition: document.querySelector(".positionIndex"),
+    cardCurrent: function() {
+        var currentCard = this.cards[this.cardIndex];
+        this.cardText.innerHTML = currentCard.display(this.cardSide);
+        this.cardPosition.innerHTML = (this.cardIndex + 1) + "/" + this.cards.length;
+    },
 
-  cardFlip: function() {
-    if (this.cardSide === 0) {
-      this.cardSide = this.cardSide + 1
+    cardFlip: function() {
+        if (this.cardSide === 0) {
+            this.cardSide = this.cardSide + 1
+        } else {
+            this.cardSide = 0
+        }
+    },
+    cardNav: function(navBy) {
+        this.cardIndex += navBy;
+        if (this.cardIndex < 0) {
+            this.cardIndex = this.cardIndex + this.cards.length;
+        } else {
+            this.cardIndex = this.cardIndex % this.cards.length;
+        }
+        //Reset next card to show front
+        this.cardSide = 0;
+        this.cardCurrent();
+    },
+    cardClick: function() {
+        this.cardFlip();
+        this.cardCurrent(); //Displays card on refresh
+    },
+    cardAdd: function(front, back) {
+        this.cards.push(new Card(front, back));
     }
-    else {
-      this.cardSide = 0
-    }
-  },
-  cardNav: function(navBy) {
-    this.cardIndex += navBy;
-    if(this.cardIndex < 0){
-      this.cardIndex += this.cards.length;
-    }
-    else {
-    this.cardIndex = this.cardIndex % this.cards.length;
-    }
-    //Reset next card to front
-    this.cardSide = 0;
-    this.cardCurrent();
-  },
-  cardClick: function() {
-    this.cardFlip();
-    this.cardCurrent();
-  },
-  cardAdd: function(front, back) {
-  this.cards.push(new Card(front, back));
-  }
 };
 
-// Add value to card for use in card
+// Add value to card for use in cardAdd
 function Card(front, back) {
-  this.frontValue = front;
-  this.backValue = back;
-  this.display = function(side) {
-    if(side === 0) {
-      return front;
-    }
-    else {
-      return back;
-    }
-  };
+    this.frontValue = front;
+    this.backValue = back;
+    this.display = function(side) {
+        if (side === 0) {
+            return front;
+        } else {
+            return back;
+        }
+    };
 }
 
+
+
 function cardMove() {
-  cardsAdmin.cardClick();
+    cardsAdmin.cardClick();
 }
 
 cardsAdmin.cardAdd("love", "αγάπη");
@@ -69,6 +68,25 @@ cardsAdmin.cardAdd("money", "χρήματα");
 cardsAdmin.cardAdd("seek", "ψάχνω");
 cardsAdmin.cardAdd("glory", "δόξα");
 cardsAdmin.cardCurrent();
+
+
+// Checks for whitespace and returns if there is
+
+// String.prototype.isEmpty = function() {
+//     return (this.length === 0 || !this.trim());
+// };
+//
+// var cardEntry = function() {
+//     var newFront = document.querySelector("#newFront");
+//     var newBack = document.querySelector("#newBack");
+//     if (newFront.value.isEmpty() || newBack.value.isEmpty())
+//         return newFront, newBack;
+//
+//     cardsAdmin.cardAdd(newFront.value, newBack.value);
+//     newFront.value = "";
+//     newBack.value = "";
+//     cardsAdmin.cardCurrent();
+// }
 
 
 cardsAdmin.cardText.addEventListener("click", cardMove);
